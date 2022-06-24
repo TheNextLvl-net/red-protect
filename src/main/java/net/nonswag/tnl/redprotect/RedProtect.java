5c724e94d95a28e638f66a54773450d64058a2ab
NonSwag <mrminecraft00@gmail.com#>
2022-06-24 17:43:12 +0200

    enhanced clock detection and performance allowed item sorting machines (from #order 3)

--- src/main/java/net/nonswag/tnl/redprotect/RedProtect.java
+++ src/main/java/net/nonswag/tnl/redprotect/RedProtect.java
@@ -1,13 +1,17 @@
 package net.nonswag.tnl.redprotect;
 
+import com.plotsquared.core.plot.Plot;
 import lombok.Getter;
 import lombok.Setter;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.hover.content.Text;
 import net.nonswag.tnl.redprotect.listeners.RedstoneListener;
 import net.nonswag.tnl.redprotect.tasks.TPSTask;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import javax.annotation.Nonnull;
@@ -49,19 +53,21 @@ public class RedProtect extends JavaPlugin {
         });
     }
 
-    private boolean messages = true;
-
-    public void broadcastMalicious(@Nonnull Location location) {
-        if (!messages) return;
-        messages = false;
-        Bukkit.getScheduler().runTaskLater(this, () -> messages = true, 50);
+    public void broadcastMalicious(@Nonnull Location location, @Nullable Plot plot) {
         Bukkit.getOnlinePlayers().forEach(all -> {
             if (!all.hasPermission("redclock.notify") || !isRedstone()) return;
             String position = location.getBlockX() + " " + location.getBlockY() + " " + location.getBlockZ();
-            all.sendMessage("§8[§4RedProtect§8] §cDetected a malicious redstone clock at §4" + position);
-            all.spigot().sendMessage(new ComponentBuilder().append("§8[§4RedProtect§8] §cClick to teleport you to the position").
-                    event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/tp " + position)).create());
+            all.sendMessage("§8[§4RedProtect§8] §cDetected a malicious redstone clock at §4" +
+                    (plot != null ? plot.getAlias().isEmpty() ? "Plot " + plot.getId() : plot.getAlias() : position));
+            all.spigot().sendMessage(new ComponentBuilder().append("§8[§4RedProtect§8] §c§nClick to teleport you to the position").
+                    event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/tp " + position)).
+                    event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text("§7Click to teleport"))).create());
         });
+        if (plot == null || plot.getOwner() == null) return;
+        Player player = Bukkit.getPlayer(plot.getOwner());
+        if (player == null) return;
+        player.sendMessage("§8[§4RedProtect§8] §cRedstone got temporarily disabled on your plot §8(§4%s§8)".
+                formatted(plot.getAlias().isEmpty() ? plot.getId().toString() : plot.getAlias()));
     }
 
     @Nonnull
--- src/main/java/net/nonswag/tnl/redprotect/listeners/RedstoneListener.java
+++ src/main/java/net/nonswag/tnl/redprotect/listeners/RedstoneListener.java
@@ -1,13 +1,16 @@
 package net.nonswag.tnl.redprotect.listeners;
 
+import com.plotsquared.core.plot.Plot;
 import net.nonswag.tnl.redprotect.RedProtect;
 import org.bukkit.Bukkit;
+import org.bukkit.Chunk;
 import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockRedstoneEvent;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -15,41 +18,79 @@ import java.util.List;
 public class RedstoneListener implements Listener {
 
     @Nonnull
-    private static final HashMap<Location, Integer> states = new HashMap<>();
+    private static final HashMap<Chunk, Integer> CHUNK_STATES = new HashMap<>();
     @Nonnull
-    private static final List<Location> sources = new ArrayList<>();
+    private static final HashMap<Plot, Integer> PLOT_STATES = new HashMap<>();
+    @Nonnull
+    private static final List<Chunk> BLOCKED_CHUNKS = new ArrayList<>();
+    @Nonnull
+    private static final List<Plot> BLOCKED_PLOTS = new ArrayList<>();
 
     @EventHandler
     public void onRedstone(@Nonnull BlockRedstoneEvent event) {
-        if (RedProtect.getInstance().isRedstone()) {
-            Location location = event.getBlock().getLocation();
-            if (increaseState(location) >= 10) {
-                if (!sources.contains(location)) {
-                    RedProtect.getInstance().broadcastMalicious(location);
-                    sources.add(location);
-                    Bukkit.getScheduler().runTaskLater(RedProtect.getInstance(), () -> sources.remove(location), 100);
-                }
-                event.setNewCurrent(0);
-            }
-            Bukkit.getScheduler().runTaskLater(RedProtect.getInstance(), () -> decreaseState(location), 100);
-        } else event.setNewCurrent(0);
+        if (!RedProtect.getInstance().isRedstone()) event.setNewCurrent(0);
+        if (event.getNewCurrent() == 0) return;
+        Location location = event.getBlock().getLocation();
+        Plot plot = plot(location);
+        if (plot != null) {
+            Bukkit.getScheduler().runTaskLater(RedProtect.getInstance(), () -> decreaseState(plot), 300);
+            if (increaseState(plot) < 5000) return;
+            event.setNewCurrent(0);
+            if (BLOCKED_PLOTS.contains(plot)) return;
+            RedProtect.getInstance().broadcastMalicious(location, plot);
+            BLOCKED_PLOTS.add(plot);
+            Bukkit.getScheduler().runTaskLater(RedProtect.getInstance(), () -> BLOCKED_PLOTS.remove(plot), 300);
+        } else {
+            Chunk chunk = location.getChunk();
+            Bukkit.getScheduler().runTaskLater(RedProtect.getInstance(), () -> decreaseState(chunk), 300);
+            if (increaseState(chunk) < 5000) return;
+            event.setNewCurrent(0);
+            if (BLOCKED_CHUNKS.contains(chunk)) return;
+            RedProtect.getInstance().broadcastMalicious(location, null);
+            BLOCKED_CHUNKS.add(chunk);
+            Bukkit.getScheduler().runTaskLater(RedProtect.getInstance(), () -> BLOCKED_CHUNKS.remove(chunk), 300);
+        }
     }
 
-    public static int increaseState(@Nonnull Location location) {
-        return setState(location, getState(location) + 1);
+    public static int increaseState(@Nonnull Chunk chunk) {
+        return setState(chunk, getState(chunk) + 1);
     }
 
-    public static int decreaseState(@Nonnull Location location) {
-        return setState(location, getState(location) - 1);
+    public static int decreaseState(@Nonnull Chunk chunk) {
+        return setState(chunk, getState(chunk) - 1);
     }
 
-    public static int setState(@Nonnull Location location, int state) {
-        if (state <= 0) states.remove(location);
-        else states.put(location, state);
-        return getState(location);
+    public static int setState(@Nonnull Chunk chunk, int state) {
+        if (state <= 0) CHUNK_STATES.remove(chunk);
+        else CHUNK_STATES.put(chunk, state);
+        return getState(chunk);
     }
 
-    public static int getState(@Nonnull Location location) {
-        return states.getOrDefault(location, 0);
+    public static int getState(@Nonnull Chunk chunk) {
+        return CHUNK_STATES.getOrDefault(chunk, 0);
+    }
+
+    public static int increaseState(@Nonnull Plot plot) {
+        return setState(plot, getState(plot) + 1);
+    }
+
+    public static int decreaseState(@Nonnull Plot plot) {
+        return setState(plot, getState(plot) - 1);
+    }
+
+    public static int setState(@Nonnull Plot plot, int state) {
+        if (state <= 0) PLOT_STATES.remove(plot);
+        else PLOT_STATES.put(plot, state);
+        return getState(plot);
+    }
+
+    public static int getState(@Nonnull Plot plot) {
+        return PLOT_STATES.getOrDefault(plot, 0);
+    }
+
+    @Nullable
+    private static Plot plot(@Nonnull Location location) {
+        if (location.getWorld() == null) return null;
+        return Plot.getPlot(com.plotsquared.core.location.Location.at(location.getWorld().getName(), location.getBlockX(), location.getBlockY(), location.getBlockZ()));
     }
 }
