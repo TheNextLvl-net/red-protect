c6e76fc3e4e6dbe58ea321e59ffbdcd816051dcd
david <github@nonswag.dev>
2025-04-20 20:07:50 +0200

    Added `toString` and `getOwner` methods to ChunkRedstoneController
    Enhanced functionality with string representation and optional owner retrieval for chunks.

--- src/main/java/net/thenextlvl/redprotect/controller/ChunkRedstoneController.java
+++ src/main/java/net/thenextlvl/redprotect/controller/ChunkRedstoneController.java
@@ -3,10 +3,23 @@ package net.thenextlvl.redprotect.controller;
 import net.thenextlvl.redprotect.RedProtect;
 import org.bukkit.Chunk;
 import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+import java.util.Optional;
 
 public class ChunkRedstoneController extends AbstractRedstoneController<Chunk> {
     public ChunkRedstoneController(RedProtect plugin) {
         super(plugin);
+    }
+
+    @Override
+    public String toString(Chunk chunk) {
+        return chunk.getX() + ", " + chunk.getZ();
+    }
+
+    @Override
+    public Optional<Player> getOwner(Chunk region) {
+        return Optional.empty();
     }
 
     @Override
