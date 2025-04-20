618a4fdb875b6632dde6242bfbfb1cce9b51c5b8
david <github@nonswag.dev>
2025-04-20 20:07:53 +0200

    Added owner retrieval in PlotRedstoneController
    Implemented `getOwner` method to fetch plot owners as `Optional<Player>`.

--- src/main/java/net/thenextlvl/redprotect/controller/PlotRedstoneController.java
+++ src/main/java/net/thenextlvl/redprotect/controller/PlotRedstoneController.java
@@ -3,10 +3,23 @@ package net.thenextlvl.redprotect.controller;
 import com.plotsquared.core.plot.Plot;
 import net.thenextlvl.redprotect.RedProtect;
 import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+import java.util.Optional;
 
 public class PlotRedstoneController extends AbstractRedstoneController<Plot> {
     public PlotRedstoneController(RedProtect plugin) {
         super(plugin);
+    }
+
+    @Override
+    public String toString(Plot region) {
+        return "";
+    }
+
+    @Override
+    public Optional<Player> getOwner(Plot plot) {
+        return Optional.ofNullable(plot.getOwner()).map(plugin.getServer()::getPlayer);
     }
 
     @Override
