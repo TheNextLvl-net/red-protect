b67d1d70ee019978b2bc1cbb66ad98d81024d5e5
david <github@nonswag.dev>
2025-04-20 20:03:34 +0200

    Added `toString` and `getOwner` methods to controller
    Implemented methods for area name conversion and owner retrieval.

--- src/main/java/net/thenextlvl/redprotect/controller/AreaRedstoneController.java
+++ src/main/java/net/thenextlvl/redprotect/controller/AreaRedstoneController.java
@@ -4,8 +4,10 @@ import net.thenextlvl.protect.area.Area;
 import net.thenextlvl.protect.area.AreaProvider;
 import net.thenextlvl.redprotect.RedProtect;
 import org.bukkit.Location;
+import org.bukkit.entity.Player;
 
 import java.util.Objects;
+import java.util.Optional;
 
 public class AreaRedstoneController extends AbstractRedstoneController<Area> {
     private final AreaProvider areaProvider;
@@ -13,6 +15,16 @@ public class AreaRedstoneController extends AbstractRedstoneController<Area> {
     public AreaRedstoneController(RedProtect plugin) {
         super(plugin);
         this.areaProvider = Objects.requireNonNull(plugin.getServer().getServicesManager().load(AreaProvider.class));
+    }
+
+    @Override
+    public String toString(Area area) {
+        return area.getName();
+    }
+
+    @Override
+    public Optional<Player> getOwner(Area area) {
+        return area.getOwner().map(plugin.getServer()::getPlayer);
     }
 
     @Override
