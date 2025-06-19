4c867d00d79516fdaf1cab8454df635a266b391e
david <github@nonswag.dev>
2025-06-19 08:04:32 +0200

    Added changelog support for Hangar publishing
    Passed `CHANGELOG` from GitHub release body to the build script for enhanced release details in Hangar publications.

--- build.gradle.kts
+++ build.gradle.kts
@@ -79,6 +79,7 @@ hangarPublish { // docs - https://docs.papermc.io/misc/hangar-publishing
     publications.register("plugin") {
         id.set("RedProtect")
         version.set(versionString)
+        changelog = System.getenv("CHANGELOG")
         channel.set(if (isRelease) "Release" else "Snapshot")
         apiKey.set(System.getenv("HANGAR_API_TOKEN"))
         platforms.register(Platforms.PAPER) {
