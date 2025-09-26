67c20e8a370ae298d8fb2e5fbc7b799ce293832e
david <mail@nonswag.dev>
2025-09-26 19:14:55 +0200

    Downgrade bstats to 3.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ dependencies {
 
     compileOnly("net.thenextlvl.protect:api:3.1.1")
 
-    implementation("org.bstats:bstats-bukkit:3.1.1-SNAPSHOT")
+    implementation("org.bstats:bstats-bukkit:3.1.0")
     implementation("net.thenextlvl.core:files:3.0.1")
     implementation("net.thenextlvl.core:i18n:3.2.2")
     implementation("net.thenextlvl.core:paper:2.3.1")
